fa62db708c5ed7a5a14fa3b687c39ea95a9306fb
Pdro Rivera <priveral@uni.pe>
2024-02-06 20:03:39 -0500

    install jest dom

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,30 @@
-export const preset = 'ts-jest';
-export const setupFilesAfterEnv = ['<rootDir>/jest.setup.js'];
-export const testPathIgnorePatterns = [
-  '<rootDir>/.next/', 
-  '<rootDir>/node_modules/'
-];
-export const moduleNameMapper = {
-  '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Este es el archivo donde configuras tu entorno de pruebas, incluyendo las importaciones de jest-dom
+  testEnvironment: 'jsdom', // 'jsdom' es el entorno de prueba recomendado para aplicaciones de React
+  moduleNameMapper: {
+    // Manejar el mapeo de módulos para CSS y cualquier otro archivo de estilo que necesites
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/', // Ignora los archivos en la carpeta .next
+    '<rootDir>/node_modules/', // Ignora los archivos en la carpeta node_modules
+  ],
+  transform: {
+    // Transforma los archivos ts/tsx con ts-jest
+    '^.+\\.(ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/'], // Indica a Jest dónde buscar módulos
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  // Agrega cualquier otra configuración que necesites aquí...
 };
-export const testEnvironment = 'node';
-export const testMatch = [
-  "**/__tests__/**/*.[jt]s?(x)",
-  "**/?(*.)+(spec|test).[tj]s?(x)"
-];
+
+module.exports = createJestConfig(customJestConfig);
